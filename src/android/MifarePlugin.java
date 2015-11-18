bd6c507548593c85d33d9645f6c4df6650dcc76c
clarklight <clarkswork@gmail.com>
2015-11-18 16:09:34 +0800

    Update MifarePlugin.java

--- src/android/MifarePlugin.java
+++ src/android/MifarePlugin.java
@@ -16,6 +16,8 @@ import com.nxp.nfclib.utils.NxpLogUtils;
 import com.nxp.nfclib.utils.Utilities;
 import com.nxp.nfcliblite.Interface.NxpNfcLibLite;
 import com.nxp.nfcliblite.Interface.Nxpnfcliblitecallback;
+import com.nxp.nfcliblite.Interface.NxpNfcLibLite;
+import com.nxp.nfcliblite.Interface.Nxpnfcliblitecallback;
 import com.nxp.nfcliblite.cards.DESFire;
 
 import com.nxp.nfcliblite.cards.Plus;
